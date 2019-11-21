d6db3c32ee071c528689a195355e3ba358970496
Spyrex <malikfk100@gmail.com>
2019-11-21 20:31:34 +0100

    Update index.php

--- index.php
+++ index.php
@@ -45,7 +45,7 @@
           $itType = $types[$row["item_type"]];
 
 
-          echo   "<li class='collection-item'><div>".$icons[$row["item_type"]]."<span>".$row["item_name"]."</span>"."<a class='secondary-content' href='#modal-".$row["id"]."'><i class='material-icons'>info</i></a></div></li>";
+          echo   "<li class='collection-item'><div>".$icons[$row["item_type"]]."".$row["item_name"].""."<a class='secondary-content' href='#modal-".$row["id"]."'><i class='material-icons'>info</i></a></div></li>";
           echo    "
           <!--ItemInfo-Popup---->
 
